e98a04a9b5d3775fe40496d93a50c628551c56ba
Bikash Singh <blackhatsys@gmail.com>
2023-03-12 07:59:51 +0530

    First Edit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,59 @@
-import React from 'react';
-import logo from './images/logo.jpg';
+import React from "react";
+import logo from "./images/logo.jpg";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { purple, indigo, blue, teal, green } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: indigo[500],
+    },
+  },
+});
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-indigo-600 to-purple-500">
-      <div className="container mx-auto flex items-center justify-between px-16 py-3">
-        <div className="flex items-center">
-          <img src={logo} alt="Logo" className="h-8 w-8 rounded-full" />
-          <h1 className="ml-2 text-lg font-medium text-white">Resume</h1>
+    <ThemeProvider theme={theme}>
+      <nav className="bg-gradient-to-r from-indigo-600 to-purple-500 via-blue-500 via-teal-500 to-green-500 h-44">
+        <div className="container mx-auto flex items-center justify-between px-34">
+          <div className="flex items-center">
+            <img src={logo} alt="Logo" className="h-16 w-16 rounded-full" />
+            <h1 className="ml-2 text-lg font-medium text-white">
+              Resume
+            </h1>
+          </div>
+          <div className="flex items-center">
+            <a
+              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100"
+              href="/about"
+            >
+              About
+            </a>
+            <a
+              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100"
+              href="/portfolio"
+            >
+              Portfolio
+            </a>
+            <a
+              className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100"
+              href="/contact"
+            >
+              Contact
+            </a>
+          </div>
         </div>
-        <div className="flex items-center">
-          <a className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100">
-            Home
-          </a>
-          <a className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100">
-            About
-          </a>
-          <a className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100">
-            Portfolio
-          </a>
-          <a className="px-3 py-2 rounded-md text-sm font-medium text-white hover:text-indigo-100 focus:outline-none focus:text-indigo-100">
-            Contact
-          </a>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </ThemeProvider>
   );
 };
 
